2ff04a3683e8153c510c1587096c6090e4eafdcf
AlfinurS <aaalfinur87@gmail.com>
2023-03-27 19:21:23 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+<div id="app">
+  <HeaderComponent></HeaderComponent>
+  <!-- <main>
+      <div class="products">
+          <div class="product-item" v-for="product in filteredProducts" :key="product.id_product">
+              <img :src="imgCatalog" alt="Some img">
+              <div class="desc">
+                  <h3>{{product.product_name}}</h3>
+                  <p>{{product.price}} $</p>
+                  <button class="buy-btn" @click="addProduct(product)">Купить</button>
+              </div>
+          </div>
+      </div>
+  </main> -->
+</div>
+</template>
+
+<script>
+  import HeaderComponent from "./components/HeaderComponent.vue"
+
+  export default {
+    name: "App",
+    components: { HeaderComponent },
+    data() {
+      return {
+        formFilter: {},
+      }
+    },
+    props: {
+      awbs: {
+        type: Object,
+      },
+      formFilterProps: {
+        type: Object,
+      }
+    },
+    methods: {
+      setFilter() {
+        this.$emit('setFilter', this.formFilter);
+      },
+      resetFilter() {
+        this.formFilter.reset();
+      },
+    },
+    mounted() {
+      this.formFilter = this.formFilterProps;
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,128 @@
+<template>
+  <header>
+    <div class="logo">E-shop</div>
+    <div class="cart">
+
+      <form action="#" class="search-form">
+        <input type="text" class="search-field" v-model="search" placeholder="Поиск товаров" >
+        <button class="btn-search" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+
+      <button class="btn-cart" type="button" @click="showCart">Корзина</button>
+
+      <div class="cart-block" v-if="isShowCart">
+        <p v-if="cart.length === 0">Корзина пустая</p>
+        <div class="cart-block-item" v-for="product in cart" :key="product.id_product">
+          <img :src="imgCatalog" alt="Some img">
+          <h3>{{product.product_name}}</h3>
+          <p>{{product.price * product.quantity}} $</p>
+          <p>{{product.quantity}} шт</p>
+          <button class="delete-btn" @click="deleteProduct(product)">Удалить</button>
+        </div>
+      </div>
+
+    </div>
+  </header>
+</template>
+  
+<script>
+  const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+  export default {
+    data() {
+      return {
+        search: "",
+        catalogUrl: `/catalogData.json`,
+        cartUrl: `/getBasket.json`,
+        imgCatalog: "https://placehold.it/200x150",
+        isShowCart: false,
+        cart: [],
+        //products: [],
+        deleteCartUrl: `/deleteFromBasket.json`,
+      }
+    },
+
+    /* props: {
+      awbs: {
+        type: String,
+        default: "",
+      },
+      formFilterProps: {
+        type: Object,
+        default: ()=>({}),
+      }
+    }, */
+
+    methods: {
+      showCart(){
+        this.isShowCart = !this.isShowCart;
+      }, 
+
+      getJson(url){
+        return fetch(url)
+          .then(result => result.json())
+          .catch(error => {
+            console.log(error);
+          })
+      },
+
+      /* addProduct(product){
+        const index = this.products.findIndex((item) => item.id_product === product.id_product);
+        let productFind = this.cart.find(product => product.id_product === this.products[index].id_product);
+        console.log(this.products);
+        if (productFind) {
+          ++productFind.quantity;
+          //this.showCart(productFind)
+        } else {
+          const cartProduct = JSON.parse(JSON.stringify(product))
+          cartProduct.quantity = 1;
+          this.cart.push(cartProduct);
+          console.log(this.cart);
+        }
+      }, */
+
+      deleteProduct(product){
+        this.getJson(`${API + this.deleteCartUrl}`)
+          .then(data => {
+            if(data.result===1){
+              const productFind = this.cart.find((item) => item.id_product === product.id_product);
+              if (productFind.quantity > 1){
+                productFind.quantity--;
+              } else {
+                this.cart.splice(this.cart.indexOf(productFind), 1);
+              }
+            }
+          })
+      },
+    },
+
+    computed: {
+      filteredProducts() {
+        if (this.search !== "") {
+          const regexp = new RegExp(this.search, 'i');
+          this.filtered = this.products.filter(product => regexp.test(product.product_name));
+          return this.filtered;
+        }
+        return this.products;
+      },
+    },
+
+    mounted(){
+      /* this.getJson(`${API + this.catalogUrl}`)
+        .then(data => {
+            this.products = [...data];
+        }) */
+
+      this.getJson(`${API + this.cartUrl}`)
+        .then(data => {
+            this.cart = data.contents;
+        })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
